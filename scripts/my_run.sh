fbd278f0b9fb01e8382998f63e506be751c51a3e
zehao-wang <zekewang@outlook.com>
2022-11-22 21:26:57 +0100

    my pose extractor

---
+++ scripts/my_run.sh
@@ -0,0 +1,3 @@
+scene_path=outputs/test_sim
+scene_path=outputs/test_sim_mulview_mulheight
+python scripts/run.py --scene $scene_path --mode nerf --gui
---
+++ scripts/my_run_nogui_sem.sh
@@ -0,0 +1,3 @@
+# scene_path=outputs/test_sim
+scene_path=outputs/test_sim_mulview_mulheight
+python scripts/run_rgbs.py --scene $scene_path --mode nerf
